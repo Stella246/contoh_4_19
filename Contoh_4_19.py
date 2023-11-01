daeffe3fce7f65f274734758cf8dac72f88d7cc3
stellakong3A <131837638+Stella246@users.noreply.github.com>
2023-11-01 15:58:54 -0700

    Update Contoh_4_19.py 1

--- Contoh_4_19.py
+++ Contoh_4_19.py
@@ -1,1 +1,22 @@
 #Contoh_4_19
+def inputPengguna(mesejInput):
+  print(mesejInput)
+  harga=float(input())
+  return harga
+
+def kiraPeratus(h1,h2):
+   peratus=((h2-h1)/h1)*100
+   peratus=round(peratus,2)
+   if peratus>0:
+     print("Keuntungan ialah",peratus,"%")
+   else:
+      print("Kerugian ialah",abs(peratus),"%")
+
+h1=inputPengguna("Masukkan harga kos RM")
+h2=inputPengguna("Masukkan harga jualan RM")
+
+if h1==h2:
+  print("Tiada keuntungan")
+else:
+  kiraPeratus(h1,h2)
+
